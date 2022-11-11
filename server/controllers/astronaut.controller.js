727c0c41ebf4bbf05c7bc09ca200e58b496a97b0
Pier2208 <p.leroy.mtl@gmail.com>
2022-11-11 18:27:30 -0500

    get an astronaut by id

--- server/controllers/astronaut.controller.js
+++ server/controllers/astronaut.controller.js
@@ -1,11 +1,23 @@
-const pool = require('../db/index');
 const router = require('express').Router();
+const pool = require('../db/index');
 
 // GET api/v1/astronauts
 router.get('/', async (req, res) => {
   try {
-    const {rows: allAstronauts} = await pool.query(`SELECT * FROM astronaut`);
-    return res.status(200).json(allAstronauts);
+    const { rows: allAstronauts } = await pool.query(`SELECT * FROM astronaut`);
+
+    if (allAstronauts.length) return res.status(200).json(allAstronauts);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// GET api/v1/astronauts/:id
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const astronaut = await pool.query(`SELECT * FROM astronaut WHERE id = $1`, [id]);
+    if (astronaut) return res.status(200).json(astronaut.rows[0]);
   } catch (err) {
     console.error(err.message);
   }
@@ -16,14 +28,12 @@ router.post('/', async (req, res) => {
   try {
     const { name } = req.body;
     const newAstronaut = await pool.query(`INSERT INTO astronaut (name) VALUES($1) RETURNING *`, [name]);
-    return res.status(200).json(newAstronaut.rows[0]);
+
+    if (newAstronaut) return res.status(200).json(newAstronaut.rows[0]);
   } catch (err) {
     console.err(err.message);
   }
 });
-
-// PUT api/v1/astronauts/:id
-router.put('/:id', (req, res) => {});
 
 // DELETE api/v1/astronauts/:id
 router.delete('/:id', (req, res) => {});
