155663c9bc8b0296ede956a9adfae4622b918579
Pier2208 <p.leroy.mtl@gmail.com>
2022-11-11 19:15:08 -0500

    added team controller + GET AND POST routes

--- server/server.js
+++ server/server.js
@@ -7,11 +7,13 @@ const app = express();
 
 // controllers
 const AstronautController = require('./controllers/astronaut.controller');
+const TeamController = require('./controllers/team.controller')
 
 // middlewares
 app.use(cors());
 app.use(express.json());
 app.use('/api/v1/astronauts', AstronautController);
+app.use('/api/v1/teams', TeamController)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
