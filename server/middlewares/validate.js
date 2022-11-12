b8cfe3772ec6a68367540ecbc03d408e4cc45881
Pier2208 <p.leroy.mtl@gmail.com>
2022-11-12 15:20:05 -0500

    validation middleware

---
+++ server/middlewares/validate.js
@@ -0,0 +1,40 @@
+const Joi = require('joi');
+
+// rules
+const name = Joi.string().max(100).required().messages({
+  'string.max': 'Le nom ne doit pas dépasser 100 caractères',
+  'string.empty': 'Le nom est requis',
+  'any.required': 'Le nom est requis'
+});
+const bio = Joi.string().allow('').max(500).messages({
+  'string.max': 'La bio ne doit pas dépasser 500 caractères'
+});
+const teamId = Joi.number().required().messages({
+  'string.empty': "Vous devez faire partie d'une équipe",
+  'any.required': "Vous devez faire partie d'une équipe"
+});
+
+module.exports = {
+  schemas: {
+    astronautSchema: Joi.object().keys({
+      name,
+      bio,
+      teamId
+    })
+  },
+  validate: schema => (req, res, next) => {
+    const { error } = schema.validate(req.body, { abortEarly: false });
+
+    if (error) {
+      let errors = [];
+      error.details.forEach(error => {
+        errors.push({
+          field: error.path[0],
+          error: error.message
+        });
+      });
+      next(errors);
+    }
+    next();
+  }
+};
