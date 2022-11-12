cdb0206b27cf4fbf1792b0098c6d548d843a30b5
Pier2208 <p.leroy.mtl@gmail.com>
2022-11-12 15:22:29 -0500

    clean up controllers

--- server/controllers/team.controller.js
+++ server/controllers/team.controller.js
@@ -2,18 +2,19 @@ const router = require('express').Router();
 const pool = require('../db/index');
 
 // GET api/v1/teams
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
   try {
     const { rows: allTeams } = await pool.query(`SELECT * FROM team`);
 
     if (allTeams.length) return res.status(200).json(allTeams);
   } catch (err) {
     console.error(err.message);
+    next(err)
   }
 });
 
 // POST api/v1/teams
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     const { name } = req.body;
     const newTeam = await pool.query(`INSERT INTO team (name) VALUES($1) RETURNING *`, [name]);
@@ -21,6 +22,7 @@ router.post('/', async (req, res) => {
     if (newTeam) return res.status(200).json(newTeam.rows[0]);
   } catch (err) {
     console.error(err.message);
+    next(err)
   }
 });
 
--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 // controllers
 const AstronautController = require('./controllers/astronaut.controller');
-const TeamController = require('./controllers/team.controller')
+const TeamController = require('./controllers/team.controller');
 
 // middlewares
 app.use(cors());
 app.use(express.json());
-app.use('/api/v1/astronauts', AstronautController);
-app.use('/api/v1/teams', TeamController)
 
-const PORT = process.env.PORT || 3001;
+app.use('/api/v1/astronauts', AstronautController);
+app.use('/api/v1/teams', TeamController);
+
+app.use((err, req, res, next) => res.status(400).json(err));
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
