8d5eec60ef407e82d0f44454b67f95c2af14c0ab
Pier2208 <p.leroy.mtl@gmail.com>
2022-11-12 15:40:32 -0500

    set up client folder

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: hotpink;
+`
+
+const App = () => {
+  return (
+    <Title>bienvenue React!</Title>
+  )
+}
+
+export default App
