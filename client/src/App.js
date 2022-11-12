81c9e7a61525eb1ddab9926fd0f89863de788cf1
Pier2208 <p.leroy.mtl@gmail.com>
2022-11-12 18:27:32 -0500

    moved Layout inside Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
-import { lazy, Suspense } from 'react'
-import * as ROUTES from "./constants/routes";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import * as ROUTES from './constants/routes';
+import Layout from './components/Layout';
 
-const Home = lazy(() => import('./pages/Home'))
-const AddAstronaut = lazy(() => import('./pages/AddAstronaut'))
-const EditAstronaut = lazy(() => import('./pages/EditAstronaut'))
-const NotFound = lazy(() => import("./pages/NotFound"));
+const Home = lazy(() => import('./pages/Home'));
+const AddAstronaut = lazy(() => import('./pages/AddAstronaut'));
+const EditAstronaut = lazy(() => import('./pages/EditAstronaut'));
+const NotFound = lazy(() => import('./pages/NotFound'));
 
 const App = () => {
   return (
     <Router>
-    <Suspense fallback={<p>Loading...</p>}>
-      <Routes>
-        <Route path={ROUTES.HOME} element={<Home />} />
-        <Route path={ROUTES.ADD} element={<AddAstronaut />} />
-        <Route path={ROUTES.EDIT} element={<EditAstronaut />} />
-        <Route component={NotFound} />
-      </Routes>
-    </Suspense>
-  </Router>
-  )
-}
+      <Layout>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Routes>
+            <Route path={ROUTES.HOME} element={<Home />} />
+            <Route path={ROUTES.ADD} element={<AddAstronaut />} />
+            <Route path={ROUTES.EDIT} element={<EditAstronaut />} />
+            <Route component={NotFound} />
+          </Routes>
+        </Suspense>
+      </Layout>
+    </Router>
+  );
+};
 
-export default App
+export default App;
