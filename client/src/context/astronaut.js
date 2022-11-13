656e6af0eabff69925de9a6a564da22b4a99bae5
Pier2208 <p.leroy.mtl@gmail.com>
2022-11-13 17:14:26 -0500

    added editAstronaut and getAstronautById fns

--- client/src/context/astronaut.js
+++ client/src/context/astronaut.js
@@ -22,11 +22,27 @@ export const AstronautProvider = ({ children }) => {
     return res.json();
   };
 
+  const editAstronaut = async (id, formData)=> {
+    console.log('formdata', formData)
+    const res = await fetch(`http://localhost:8080/api/v1/astronauts/${id}`, {
+      method: 'PUT',
+      body: formData
+    });
+    return res.json();
+  };
+
+  const getAstronautById = async id => {
+    const res = await fetch(`http://localhost:8080/api/v1/astronauts/${id}`);
+    return res.json();
+  };
+
   return (
     <AstronautContext.Provider
       value={{
         getAllAstronauts,
-        addAstronaut
+        addAstronaut,
+        editAstronaut,
+        getAstronautById
       }}
     >
       {children}
