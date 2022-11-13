ad50fd19d210eb206958fa3f696dc90d10e2e42a
Pier2208 <p.leroy.mtl@gmail.com>
2022-11-13 11:31:10 -0500

    dynamic add btn in navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-import styled from 'styled-components'
-import AddButton from '../assets/addButton.svg'
-import * as ROUTES from "../constants/routes";
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import styled from 'styled-components';
+import AddButton from '../assets/addButton.svg';
+import * as ROUTES from '../constants/routes';
 
 const Navbar = () => {
+  let { pathname } = useLocation();
+
   return (
     <Header>
-        <Link to={ROUTES.HOME}>Eleven-Labs</Link>
+      <Link to={ROUTES.HOME}>Eleven-Labs</Link>
+      {pathname !== ROUTES.ADD && (
         <Link to={ROUTES.ADD}>
-            <img src={AddButton} alt="Add a new astronaut" />
+          <img src={AddButton} alt='Add a new astronaut' />
         </Link>
+      )}
     </Header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
 
 const Header = styled.header`
-    background: var(--color-primary);
-    color: var(--white);
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: var(--spacing-s) var(--spacing-m);
-`
+  background: var(--color-primary);
+  color: var(--white);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: var(--spacing-s) var(--spacing-m);
+`;
