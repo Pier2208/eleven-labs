d8af37341bba5626ca2778184822d4ba66f1afc8
Pier2208 <p.leroy.mtl@gmail.com>
2022-11-15 15:03:31 -0500

    added ssl key in prod config

--- db/index.js
+++ db/index.js
@@ -3,7 +3,12 @@ require('dotenv').config();
 
 const config = {
   dev: { user: process.env.USER_DB, password: process.env.PASSWORD_DB, host: process.env.HOST_DB, port: process.env.PORT_DB, database: process.env.DATABASE },
-  prod: { connectionString: process.env.DATABASE_URL }
+  prod: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  }
 };
 const pool = new Pool(process.env.NODE_ENV === 'production' ? config.prod : config.dev);
 
