c08079ee9618a881f993a71ba046dd00d64802ac
Pier2208 <p.leroy.mtl@gmail.com>
2022-11-15 14:09:05 -0500

    change sql script

--- script.sql
+++ script.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS public."team"
+CREATE TABLE IF NOT EXISTS team
 (
     id SERIAL PRIMARY KEY,
     name character varying(100)
 );
 
-INSERT INTO public."team"(id, name) 
+INSERT INTO team(id, name) 
 VALUES(1, 'Les explorateurs'), (2, 'Les mangeurs de chips'), (3, 'Les couch potatoes'), (4, 'Les Martiens'), (5, 'Les anciens astronautes');
 
 
-CREATE TABLE IF NOT EXISTS public."astronaut"
+CREATE TABLE IF NOT EXISTS astronaut
 (
     id SERIAL PRIMARY KEY,
     name character varying(100),
